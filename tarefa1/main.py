c3f781b5dd25e7e9c193359c230662e8526bf6ae
Gabriel <gabrieldutra@id.uff.br>
2018-04-01 19:47:47 -0300

    adds first somatorio feature

--- tarefa1/main.py
+++ tarefa1/main.py
@@ -21,9 +21,26 @@ def headers():
     return render_template('headers.html', headers=headers)
 
 
+@app.route('/tarefa1/somatorio')
+def somatorio():
+    try:
+        start = request.args.get('inicio', type=int)
+        end = request.args.get('fim', type=int)
+        if not (start and end):
+            raise KeyError
+        if start > end:
+            raise ValueError
+    except KeyError:
+        return render_template('error_message.html', message='São necessários os parâmetros "inicio" e "fim"')
+    except ValueError:
+        return render_template('error_message.html', message='Valores inválidos para início e fim!')
+    sum_result = sum(range(start, end+1))
+    return render_template('somatorio.html', start=start, end=end, sum_result=sum_result)
+
+
 def main():
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    main()
